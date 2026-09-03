2848fd0f6e365d354f78481ff49f0a17a16f5f97
Vitor Ribeiro <vitorribeirodev@gmail.com>
2026-09-03 10:05:20 -0300

    Add colors.py file

---
+++ src/utils/colors.py
@@ -0,0 +1,12 @@
+"""
+    Este arquivo define os códigos das cores usadas no sistema
+"""
+class Palette:
+    # Cores
+    BACKGROUND = "#eaeaea"
+
+    @classmethod
+    # Converte Hex para RGB normalizado (0.0 a 1.0) para uso no OpenGL
+    def hex_to_rgb_normalized(cls, hex_code: str) -> tuple[float, float, float]:
+        hex_code = hex_code.lstrip('#')
+        return tuple(int(hex_code[i:i+2], 16) / 255.0 for i in (0, 2, 4))
